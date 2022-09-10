b798bb2c88a51368c67c762bee93870c0a15f056
Tachidito <85906328+AxMar1221@users.noreply.github.com>
2022-09-09 19:05:44 -0500

    validation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
@@ -23,7 +23,8 @@ import { NavbarComponent } from './pages/navbar/navbar.component';
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/pages/reactive/reactive.component.ts
+++ src/app/pages/reactive/reactive.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-reactive',
@@ -6,10 +7,75 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./reactive.component.css']
 })
 export class ReactiveComponent implements OnInit {
+  form!: FormGroup;
 
-  constructor() { }
+
+  constructor( private fb: FormBuilder) {
+    this.createForm();
+    this.loadDataToForm();
+  }
 
   ngOnInit(): void {
   }
-
+  get hobies(){
+    return this.form.get('hobies') as FormArray
+  }
+  get nameNoValid(){
+    return this.form.get('name')?.invalid && this.form.get('name')?.touched
+  }
+  get lastnameNoValid(){
+    return this.form.get('lastname')?.invalid && this.form.get('lastname')?.touched
+  }get emailNoValid(){
+    return this.form.get('email')?.invalid && this.form.get('email')?.touched
+  }
+  get cityNoValid(){
+    return this.form.get('adress.city')?.invalid && this.form.get('adress.city')?.touched
+  }
+  get estatelNoValid(){
+    return this.form.get('adress.estate')?.invalid && this.form.get('adress.estate')?.touched
+  }
+  createForm(){
+    this.form = this.fb.group({
+      name:     ['', [Validators.required, Validators.minLength(4)]],
+      lastname: ['', [Validators.required, Validators.minLength(3)]],
+      email:    ['', [Validators.required, Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$')]],
+      adress: this.fb.group({
+        city: ['', Validators.required],
+        estate: ['', Validators.required],
+      }),
+      hobies: this.fb.array([])
+    });
+  }
+  loadDataToForm(){
+    this.form.reset({
+      name: 'Mario',
+      lastname: 'Hernández',
+      email: 'tachidito_7inc@hotmail.com',
+      adress: {
+        city: 'Puebla',
+        estate: 'Puebla',
+      }
+    })
+  }
+  addHobie(){
+    this.hobies.push( this.fb.control(''))
+  }
+  deletHobie(i:number){
+    this.hobies.removeAt(i);
+  }
+  save() {
+    console.log(this.form)
+    if(this.form.invalid){
+      return Object.values(this.form.controls).forEach( control => {
+        if( control instanceof FormGroup ){
+          Object.values( control.controls ).forEach( control => control.markAsTouched());
+        }else{
+          control.markAllAsTouched();
+        }
+      });
+  }
+  this.form.reset({
+    name: '',
+  });
+}
 }
